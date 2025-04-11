62053dc8e086ffad1d8a0a4d0b23a0c666ad77fd
murillobazz <murillobazilio@gmail.com>
2025-04-10 22:22:42 -0300

    add error page2

--- app/error.js
+++ app/error.js
@@ -10,7 +10,7 @@ export default function Error({ error, reset }) {
   }, [error]);
  
   return (
-    <div>
+    <div className="max-w-3xl mx-auto flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-8 sm:p-16">
       <h2>Something went wrong!</h2>
       <Link
         className="rounded-full bg-(--spotify-green) py-2 px-4 cursor-pointer hover:scale-[1.05] transition duration-300 ease-in"
